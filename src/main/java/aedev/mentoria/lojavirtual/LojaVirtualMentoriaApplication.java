23b54844c642212edbe9cda86262e3c60824a7b2
ailan <ailan@Eveli>
2025-10-19 22:18:12 -0300

    criado classe de marca produto

--- src/main/java/aedev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/aedev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,11 @@ package aedev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "devjava.lojavirtual.model")
+
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
